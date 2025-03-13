da0ef67734e8aa1e52bdb45ac29cc07be3467ce5
Luis Isaac Cuautle Delgadillo <cuautle.tron@gmail.com>
2025-03-13 11:13:37 -0600

    Actualizando el paciente

--- src/store.ts
+++ src/store.ts
@@ -1,66 +1,58 @@
-import { create } from "zustand"
-import { v4 as uuidV4 } from "uuid"
-import { devtools } from 'zustand/middleware'
+import { create } from "zustand";
+import { v4 as uuidV4 } from "uuid";
+import { devtools } from "zustand/middleware";
 
-import { DraftPatient, Patient } from "./types"
-
+import { DraftPatient, Patient } from "./types";
 
 type PatientState = {
-
-    patients: Patient[]
-    activeId: Patient['id']
-    addPatient: (data: DraftPatient) => void
-    deletePatient: (id: Patient['id']) => void
-    getPatientById: (id: Patient['id']) => void
-
-}
+  patients: Patient[];
+  activeId: Patient["id"];
+  addPatient: (data: DraftPatient) => void;
+  deletePatient: (id: Patient["id"]) => void;
+  getPatientById: (id: Patient["id"]) => void;
+  updatePatient: (data: DraftPatient) => void;
+};
 
 const createPatient = (patient: DraftPatient): Patient => {
+  return { ...patient, id: uuidV4() };
+};
 
-    return { ...patient, id: uuidV4() }
+export const usePatientStore = create<PatientState>()(
+  devtools((set) => ({
+    patients: [],
 
-}
+    activeId: "",
 
-export const usePatientStore = create<PatientState>()( 
-    devtools((set) => ({
+    // Modifica el state
+    addPatient: (data) => {
+      const newPatient = createPatient(data);
 
-        patients: [],
+      set((state) => ({
+        patients: [...state.patients, newPatient],
+      }));
+    },
 
-        activeId: '',
+    deletePatient: (id) => {
+      set((state) => ({
+        patients: state.patients.filter((patient) => patient.id !== id),
+      }));
+    },
 
-        // Modifica el state
-        addPatient: (data) => {
+    getPatientById: (id) => {
+      set(() => ({
+        activeId: id,
+      }));
+    },
 
-            const newPatient = createPatient(data)
-
-            set((state) => ({
-
-                patients: [...state.patients, newPatient]
-
-            }))
-
-        },
-
-        deletePatient: (id) => {
-
-            set((state) =>
-            ({
-
-                patients: state.patients.filter(patient => patient.id !== id)
-
-            }))
-
-        },
-
-        getPatientById: (id) => {
-
-            set(() => ({
-
-                activeId: id
-
-            }))
-
-        },
-
-    }))
-)
+    updatePatient: (data) => {
+      set((state) => ({
+        patients: state.patients.map((patient) =>
+          patient.id === state.activeId
+            ? { id: state.activeId, ...data }
+            : patient
+        ),
+        activeId: "",
+      }));
+    },
+  }))
+);
